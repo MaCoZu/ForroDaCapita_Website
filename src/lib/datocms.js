cca4ab21b4f4348d0c81af40c64bc240b5b93f6e
MaCoZu <marcoz@posteo.de>
2025-06-27 19:54:57 +0200

    import { DATOCMS_API_KEY } from 'astro:env/server'

--- src/lib/datocms.js
+++ src/lib/datocms.js
@@ -1,6 +1,5 @@
 import { executeQuery as libExecuteQuery } from '@datocms/cda-client'
 import { DATOCMS_API_KEY } from 'astro:env/server'
-console.log('DATOCMS_API_KEY:', DATOCMS_API_KEY)
 
 export async function executeQuery(query, options) {
   return await libExecuteQuery(query, {
