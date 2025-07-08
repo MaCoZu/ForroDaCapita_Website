6f7c47f1b5a86aea0e092b78eb01d9d18287925c
MaCoZu <marcoz@posteo.de>
2025-07-08 09:42:42 +0200

    remove 'serverless'

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import react from '@astrojs/react'
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig, envField } from 'astro/config'
 
