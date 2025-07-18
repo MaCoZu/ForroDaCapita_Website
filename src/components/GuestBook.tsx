ce7e138c9d36eb3cf96a46e7b7b23c549937c9c4
MaCoZu <marcoz@posteo.de>
2025-07-18 12:12:37 +0200

    new buttons & placeholder transparency

--- src/components/GuestBook.tsx
+++ src/components/GuestBook.tsx
@@ -1,6 +1,6 @@
 import DOMPurify from 'dompurify'
 import { marked } from 'marked'
-import type { FC, FormEvent } from 'react'
+import type { ChangeEvent, FC, FormEvent } from 'react'
 import { useEffect, useRef, useState } from 'react'
 import { twMerge } from 'tailwind-merge'
 
@@ -12,8 +12,58 @@ const formatDate = (dateString: string) => {
   return `${day} ${month}. ${year}`
 }
 
+
+
+// Type for marked renderer link function parameters
+interface LinkRendererParams {
+  href?: string
+  title?: string | null
+  text?: string
+}
+
 // Custom renderer for marked to handle links and headings properly
 const renderer = new marked.Renderer()
+
+renderer.list = (token: List) => {
+  const items = token.items.map(item => {
+    if (item.task) {
+      return `<li><input type="checkbox" ${item.checked ? 'checked' : ''} disabled>${item.text}</input></li>`;
+    } else {
+      return `<li>${item.text}</li>`;
+    }
+  }).join('\n');
+
+  if (token.ordered) {
+    let start = '';
+    if (token.start !== 1 && token.start !== undefined) {
+      start = ` start="${token.start}"`;
+    }
+    return `<ol${start}>\n${items}\n</ol>`;
+  } else {
+    return `<ul>\n${items}\n</ul>`;
+  }
+};
+
+// Customize image rendering
+renderer.image = (href: string | null, title: string | null, text: string) => {
+  if (!href) return text
+  const sanitizedHref = DOMPurify.sanitize(href)
+  const sanitizedText = DOMPurify.sanitize(text)
+  const sanitizedTitle = title ? DOMPurify.sanitize(title) : null
+
+  const attributes = {
+    src: sanitizedHref,
+    alt: sanitizedText,
+    ...(sanitizedTitle && { title: sanitizedTitle }),
+  }
+
+  const attributeString = Object.entries(attributes)
+    .map(([key, value]) => `${key}="${value}"`)
+    .join(' ')
+
+  return `<img ${attributeString} class="custom-image-class" />`
+}
+
 
 // Enhanced link renderer with better security and accessibility
 renderer.link = function ({ href = '', title, text = '' }: LinkRendererParams) {
@@ -84,19 +134,6 @@ interface MessageBoardProps {
   containerClasses?: string
 }
 
-// Type for marked renderer link function parameters
-interface LinkRendererParams {
-  href?: string
-  title?: string | null
-  text?: string
-}
-
-// Type for marked renderer heading function parameters
-interface HeadingRendererParams {
-  text: string
-  depth: number
-}
-
 const MessageBoard: FC<MessageBoardProps> = ({ containerClasses }) => {
   const [messages, setMessages] = useState<Message[]>([])
   const [message, setMessage] = useState('')
@@ -107,6 +144,8 @@ const MessageBoard: FC<MessageBoardProps> = ({ containerClasses }) => {
 
   const baseContentClasses = 'flex items-center justify-center w-full'
   const mergedContentClasses = twMerge(baseContentClasses, containerClasses)
+
+  
 
   // Fetch messages from server-side API
   async function fetchMessages() {
@@ -250,8 +289,76 @@ const MessageBoard: FC<MessageBoardProps> = ({ containerClasses }) => {
           Guest Book
         </h2>
 
+        {/* Markdown Buttons  */}
         <form onSubmit={handleSubmit} className="flex flex-col">
           <div className="right-0 mt-2 flex justify-end gap-3">
+            {/* H1  */}
+            <button
+              type="button"
+              className=""
+              onClick={() => {
+                const start = message.slice(
+                  0,
+                  textareaRef.current?.selectionStart ?? 0
+                )
+                const end = message.slice(
+                  textareaRef.current?.selectionEnd ?? 0
+                )
+                setMessage(`${start}# Heading 1\n${end}`)
+                setTimeout(() => {
+                  textareaRef.current?.focus()
+                }, 0)
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+              >
+                <g fill="none">
+                  <path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+                  <path
+                    fill="currentColor"
+                    d="M13 2.5a1.5 1.5 0 0 1 1.493 1.356L14.5 4v16a1.5 1.5 0 0 1-2.993.144L11.5 20v-6.5h-6V20a1.5 1.5 0 0 1-2.993.144L2.5 20V4a1.5 1.5 0 0 1 2.993-.144L5.5 4v6.5h6V4A1.5 1.5 0 0 1 13 2.5m6 11.019V20a1 1 0 1 1-2 0v-4.634a1 1 0 0 1-1.055-1.698l1.485-.99a1.01 1.01 0 0 1 1.57.84Z"
+                  />
+                </g>
+              </svg>
+            </button>
+            {/* H2  */}
+            <button
+              type="button"
+              className=""
+              onClick={() => {
+                const start = message.slice(
+                  0,
+                  textareaRef.current?.selectionStart ?? 0
+                )
+                const end = message.slice(
+                  textareaRef.current?.selectionEnd ?? 0
+                )
+                setMessage(`${start}## Heading 2\n${end}`)
+                setTimeout(() => {
+                  textareaRef.current?.focus()
+                }, 0)
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+              >
+                <g fill="none">
+                  <path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+                  <path
+                    fill="currentColor"
+                    d="M13 2.5a1.5 1.5 0 0 1 1.493 1.356L14.5 4v16a1.5 1.5 0 0 1-2.993.144L11.5 20v-6.5h-6V20a1.5 1.5 0 0 1-2.993.144L2.5 20V4a1.5 1.5 0 0 1 2.993-.144L5.5 4v6.5h6V4A1.5 1.5 0 0 1 13 2.5m4.657 10.18a2.7 2.7 0 0 1 1.873.219c1.166.582 1.757 1.92 1.443 3.177a2.8 2.8 0 0 1-.537 1.08l-.133.153L18.724 19h1.63a1 1 0 0 1 .117 1.993l-.117.007H16.65a1.01 1.01 0 0 1-1.01-1.01c0-.292.023-.569.191-.807l.081-.1l2.93-3.138c.472-.507.05-1.513-.699-1.325c-.313.078-.466.335-.496.626l-.006.111a1 1 0 1 1-2 0c0-1.246.782-2.369 2.017-2.678Z"
+                  />
+                </g>
+              </svg>
+            </button>
+      
             {/* bold  */}
             <button
               type="button"
@@ -272,14 +379,17 @@ const MessageBoard: FC<MessageBoardProps> = ({ containerClasses }) => {
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="1.5em"
-                height="1.5em"
-                viewBox="0 0 256 256"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
               >
-                <path
-                  fill="currentColor"
-                  d="M185.08 114.46A48 48 0 0 0 148 36H80a12 12 0 0 0-12 12v152a12 12 0 0 0 12 12h80a52 52 0 0 0 25.08-97.54M92 60h56a24 24 0 0 1 0 48H92Zm68 128H92v-56h68a28 28 0 0 1 0 56"
-                />
+                <g fill="none">
+                  <path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+                  <path
+                    fill="currentColor"
+                    d="M13 2.5a5.5 5.5 0 0 1 4.213 9.036a5.5 5.5 0 0 1-2.992 9.96L14 21.5H6.1a1.6 1.6 0 0 1-1.593-1.454L4.5 19.9V4.1a1.6 1.6 0 0 1 1.454-1.593L6.1 2.5zm1 11H7.5v5H14a2.5 2.5 0 0 0 0-5m-1-8H7.5v5H13a2.5 2.5 0 0 0 0-5"
+                  />
+                </g>
               </svg>
             </button>
             {/* italic  */}
@@ -302,14 +412,117 @@ const MessageBoard: FC<MessageBoardProps> = ({ containerClasses }) => {
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="1.5em"
-                height="1.5em"
-                viewBox="0 0 256 256"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
               >
-                <path
-                  fill="currentColor"
-                  d="M204 56a12 12 0 0 1-12 12h-31.35l-40 120H144a12 12 0 0 1 0 24H64a12 12 0 0 1 0-24h31.35l40-120H112a12 12 0 0 1 0-24h80a12 12 0 0 1 12 12"
-                />
+                <g fill="none">
+                  <path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+                  <path
+                    fill="currentColor"
+                    d="M9 4a1 1 0 0 1 1-1h6a1 1 0 1 1 0 2h-2.117l-1.75 14H14a1 1 0 1 1 0 2H8a1 1 0 1 1 0-2h2.117l1.75-14H10a1 1 0 0 1-1-1"
+                  />
+                </g>
+              </svg>
+            </button>
+
+            {/* bolditalic  */}
+            <button
+              type="button"
+              className=""
+              onClick={() => {
+                const start = message.slice(
+                  0,
+                  textareaRef.current?.selectionStart ?? 0
+                )
+                const end = message.slice(
+                  textareaRef.current?.selectionEnd ?? 0
+                )
+                setMessage(`${start}***bold italics***${end}`)
+                setTimeout(() => {
+                  textareaRef.current?.focus()
+                }, 0)
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+              >
+                <g fill="none">
+                  <path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+                  <path
+                    fill="currentColor"
+                    d="M16 2.5h-6a1.5 1.5 0 0 0 0 3h1.3l-1.624 13H8a1.5 1.5 0 0 0 0 3h6a1.5 1.5 0 0 0 0-3h-1.3l1.624-13H16a1.5 1.5 0 0 0 0-3"
+                  />
+                </g>
+              </svg>
+            </button>
+            {/* striketrough  */}
+            <button
+              type="button"
+              className=""
+              onClick={() => {
+                const start = message.slice(
+                  0,
+                  textareaRef.current?.selectionStart ?? 0
+                )
+                const end = message.slice(
+                  textareaRef.current?.selectionEnd ?? 0
+                )
+                setMessage(`${start}~striketrough~${end}`)
+                setTimeout(() => {
+                  textareaRef.current?.focus()
+                }, 0)
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+              >
+                <g fill="none">
+                  <path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+                  <path
+                    fill="currentColor"
+                    d="M19 12a1 1 0 0 1 .117 1.993L19 14h-2.11q.327.492.518 1.063c.953 2.859-1.109 5.809-4.083 5.933L13.13 21h-1.702a5.74 5.74 0 0 1-5.02-2.958l-.112-.213l-.182-.365a1 1 0 0 1-.105-.6a1 1 0 0 1 1.817-.427l.042.066l.217.431a3.74 3.74 0 0 0 3.131 2.06l.212.006h1.701a2.51 2.51 0 0 0 1.297-4.66l-.174-.096l-.488-.244H5a1 1 0 0 1-.117-1.993L5 12zm-6.428-9a5.74 5.74 0 0 1 5.132 3.171l.18.363a1.002 1.002 0 0 1-1.405 1.32a1 1 0 0 1-.316-.306l-.035-.058l-.213-.424A3.74 3.74 0 0 0 12.572 5h-1.701a2.51 2.51 0 0 0-1.123 4.756L12.236 11H8.013A4.5 4.5 0 0 1 6.59 8.937C5.618 6.017 7.791 3 10.871 3z"
+                  />
+                </g>
+              </svg>
+            </button>
+            {/* dividing line  */}
+            <button
+              type="button"
+              className=""
+              onClick={() => {
+                const start = message.slice(
+                  0,
+                  textareaRef.current?.selectionStart ?? 0
+                )
+                const end = message.slice(
+                  textareaRef.current?.selectionEnd ?? 0
+                )
+                setMessage(`${start}\n\n---\n\n${end}`)
+                setTimeout(() => {
+                  textareaRef.current?.focus()
+                }, 0)
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+              >
+                <g fill="none">
+                  <path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+                  <path
+                    fill="currentColor"
+                    d="M6 18a1 1 0 1 1 0 2H4a1 1 0 1 1 0-2zm7 0a1 1 0 1 1 0 2h-2a1 1 0 1 1 0-2zm7 0a1 1 0 1 1 0 2h-2a1 1 0 1 1 0-2zm0-7a1 1 0 0 1 .117 1.993L20 13H4a1 1 0 0 1-.117-1.993L4 11zM6 4a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm7 0a1 1 0 0 1 .117 1.993L13 6h-2a1 1 0 0 1-.117-1.993L11 4zm7 0a1 1 0 0 1 .117 1.993L20 6h-2a1 1 0 0 1-.117-1.993L18 4z"
+                  />
+                </g>
               </svg>
             </button>
             {/* soft return  */}
@@ -346,10 +559,12 @@ const MessageBoard: FC<MessageBoardProps> = ({ containerClasses }) => {
 
           <textarea
             ref={textareaRef}
-            className="textarea textarea-bordered text-base-content bg-base-100 placeholder-base-content/80 focus:ring-base-content mt-1 h-28 w-full rounded-md text-lg focus:border-transparent focus:ring-1 focus:outline-none md:h-18"
+            className="textarea textarea-bordered text-base-content bg-base-100 placeholder-base-content/40 focus:ring-base-content mt-1 h-28 w-full rounded-md text-lg focus:border-transparent focus:ring-1 focus:outline-none md:h-18"
             placeholder={`Feedback, wish, or link to your playlist.\nUse Markdown formatting or the buttons above.`}
             value={message}
-            onChange={(e) => setMessage(e.target.value)}
+            onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+              setMessage(e.target.value)
+            }
           ></textarea>
           <button
             className="btn bg-base-200 my-2 rounded-md font-bold"
